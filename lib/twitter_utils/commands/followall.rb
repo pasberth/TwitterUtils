36dfe891f782867d7bac4a82804bc75f6504cbcc
pasberth <pasberth@gmail.com>
2012-05-11 14:43:32 +0900

    followall command.

--- lib/twitter_utils/commands/followall.rb
+++ lib/twitter_utils/commands/followall.rb
@@ -1,5 +1,4 @@
 require 'twitter_utils/commands/base'
-require 'pp'
 require 'give4each'
 
 module TwitterUtils::Commands
@@ -8,16 +7,24 @@ module TwitterUtils::Commands
     option ['--tags'], 'TAGS', 'searching hash-tags to follow.', :default => [] do |tags|
       tags.split(',').map &:+.in('#') 
     end
+
     option ['--cluster'], 'CLUSTER', "searching words in each user's bio to follow.", :default => [] do |words|
       words.split(/[\,\/]/)
     end
+
+    # option ['-f', '--force'], :flag, "force following. (NO ALL WARNING.)", :default => false
     
     def execute
       followed_user_ids = api.friend_ids.collection
 
       hash_tagged_user_ids = tags.map do |tag|
-        api.search(tag).map &:from_user_id
-      end.flatten.uniq.reject &:include?.in( followed_user_ids )
+        [].tap do |r|
+          begin
+            ids = api.search(tag, :rpp => 100).map &:from_user_id
+            r.concat ids
+          end while ids.count == 100
+        end
+      end.flatten.uniq.reject &:include?.in( [api.user.id, *followed_user_ids] )
       
       users = hash_tagged_user_ids.map { |id| api.user(id) }
       users.select! do |user|
@@ -25,15 +32,22 @@ module TwitterUtils::Commands
           user.description =~ /#{Regexp.quote c}/i
         end
       end
-
+      
       puts "This program will follow #{users.count} users:"
-      pp users.map &:screen_name
+      users.each do |user|
+        puts "%s%s: %d tweets. %d friends. %d followers." % [
+          user.screen_name,
+          ' ' * (20 - user.screen_name.length),
+          user.statuses_count,
+          user.friends_count,
+          user.followers_count]
+      end
 
       loop do
         puts "Do you follow them really? [Y/N]"
         case STDIN.gets.chomp
         when /Y/i, /Yes/i
-          puts "unimplemented."
+          users.each { |user| api.follow(user.id) }
           break
         when /N/i, /No/i
           puts "was canceled."
